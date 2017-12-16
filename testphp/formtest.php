0bd5d443cbf2bee848e9419ec323960ace78cf23
Doug Gleichman <douglas.gleichman@thinkom.com>
2017-12-16 15:11:43 -0800

    More graphics, organize php

--- testphp/formtest.php
+++ testphp/formtest.php
@@ -4,12 +4,14 @@
  </head>
  <body>
  
+	<h1>Test Forms</h1>
+	<p>This examples tests forms</p>
 
-<form action="action.php" method="post">
- <p>Your name: <input type="text" name="name" /></p>
- <p>Your age: <input type="text" name="age" /></p>
- <p><input type="submit" /></p>
-</form>
+	<form action="action.php" method="post">
+	 <p>Your name: <input type="text" name="name" /></p>
+	 <p>Your age: <input type="text" name="age" /></p>
+	 <p><input type="submit" /></p>
+	</form>
 
 
 

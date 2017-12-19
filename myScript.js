6ac7748b086d94b450b3493a7eb7d0dcc4fc72fe
Doug Gleichman <douglas.gleichman@thinkom.com>
2017-12-19 09:39:06 -0800

    Scaled color and chart

--- myScript.js
+++ myScript.js
@@ -17,3 +17,24 @@ function rgbColor(r, g, b){
   b = Math.floor(b);
   return ["rgb(",r,",",g,",",b,")"].join("");
 }
+
+function rgbScaled(r, g, b){
+	if (r > 1.0)
+		r = 1.0;
+	else if (r < 0.0)
+		r = 0.0;
+	if (g > 1.0)
+		g = 1.0;
+	else if (g < 0.0)
+		g = 0.0;
+	if (b > 1.0)
+		b = 1.0;
+	else if (b < 0.0)
+		b = 0.0;
+	r = 255.0 * r;
+	g = 255.0 * g;
+	b = 255.0 * b;
+	return rgbColor(r,g,b);
+}
+
+	

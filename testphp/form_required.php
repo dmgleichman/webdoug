e54e1704be986f6fd6e45130734a00b96496e254
Doug Gleichman <douglas.gleichman@thinkom.com>
2017-12-27 10:36:43 -0800

    PHP Forms complete validation example

--- testphp/form_required.php
+++ testphp/form_required.php
@@ -9,19 +9,46 @@
  <body>
  
 	<h1>Form Validation</h1>
-	<p>Simple</p>
+
 	
 	<?php
 	// define variables and set to empty values
+	$nameErr = $emailErr = $genderErr = $websiteErr = "";
 	$name = $email = $gender = $comment = $website = "";
 
+	// handle user input
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	  $name = test_input($_POST["name"]);
-	  $email = test_input($_POST["email"]);
-	  $website = test_input($_POST["website"]);
-	  $comment = test_input($_POST["comment"]);
-	  $gender = test_input($_POST["gender"]);
+		if (empty($_POST["name"])) {
+			$nameErr = "Name is required";
+		} else {
+			$name = test_input($_POST["name"]);
+		}
+	  
+		if (empty($_POST["email"])) {
+			$emailErr = "Email is required";
+		} else {
+			$email = test_input($_POST["email"]);
+		}
+
+		if (empty($_POST["website"])) {
+			$website = "";
+		} else {
+			$website = test_input($_POST["website"]);
+		}
+
+		if (empty($_POST["comment"])) {
+			$comment = "";
+		} else {
+			$comment = test_input($_POST["comment"]);
+		}
+
+		if (empty($_POST["gender"])) {
+			$genderErr = "Gender is required";
+		} else {
+			$gender = test_input($_POST["gender"]);
+		}
 	}
+
 
 	function test_input($data) {
 	  $data = trim($data);
@@ -31,19 +58,28 @@
 	}
 	?>
 
+	<!-- Start of static page here -->
+	
 	<h2>PHP Form Validation Example</h2>
+	
+	<p><span class="error">* required field.</span></p>
+	
 	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
 	  Name: <input type="text" name="name">
+	  <span class="error">* <?php echo $nameErr;?></span>
 	  <br><br>
 	  E-mail: <input type="text" name="email">
+	  <span class="error">* <?php echo $emailErr;?></span>
 	  <br><br>
 	  Website: <input type="text" name="website">
+	  <span class="error"><?php echo $websiteErr;?></span>
 	  <br><br>
 	  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
 	  <br><br>
 	  Gender:
 	  <input type="radio" name="gender" value="female">Female
 	  <input type="radio" name="gender" value="male">Male
+	  <span class="error">* <?php echo $genderErr;?></span>
 	  <br><br>
 	  <input type="submit" name="submit" value="Submit">  
 	</form>

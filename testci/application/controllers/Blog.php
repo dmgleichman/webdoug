1721be9fd4b8e9bef86005b2f7625ee8ae53c072
Douglas Gleichman <douglas.gleichman@thinkom.com>
2018-01-05 18:10:27 -0800

    Codeigniter user manual General Topics starting

---
+++ testci/application/controllers/Blog.php
@@ -0,0 +1,20 @@
+<?php
+class Blog extends CI_Controller {
+	
+	public function index()
+	{
+		echo 'Hello World!';
+	}
+	
+	public function comments()
+	{
+		echo 'Look at this!';
+	}
+	
+	public function silly()
+	{
+		echo 'You keep making silly commands!';
+	}
+
+	
+}

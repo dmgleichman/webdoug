36663544b418e459ec545af7e9c47bb04746b4c7
Doug Gleichman <douglas.gleichman@thinkom.com>
2018-01-07 17:33:42 -0800

    Show Blog example normal and italics

--- testci/application/controllers/Blog.php
+++ testci/application/controllers/Blog.php
@@ -1,21 +1,23 @@
 <?php
 class Blog extends CI_Controller {
 	
-	public function index()
+	
+	private function setup_blog()
 	{
-		//echo 'Hello World!';
-		
 		$data['todo_list'] = array('Clean House', 'Call Mom', 'Run Errands');
 		$data['shopping_list'] = array('Milk', 'Eggs', 'Bread', 'Bananas');
 		
 		$data['title'] = "My Real Title";
 		$data['heading'] = "My Real Heading";
+		return $data;
+	}
+	
+	public function index()
+	{
+		$data = $this->setup_blog();
 		
-		$string = $this->load->view('blog_view', $data, TRUE);
-		
-		$data['string'] = $string;
-		
-		$this->load->view('simple_view', $data);
+		$this->load->view('blog_view', $data);
+	
 	}
 	
 	public function comments()
@@ -28,5 +30,15 @@ class Blog extends CI_Controller {
 		echo 'You keep making silly commands!';
 	}
 
+	public function in_italics()
+	{
+		$data = $this->setup_blog();
+		
+		$string = $this->load->view('blog_view', $data, TRUE);	// returns view as a string
+		
+		$data['string'] = $string;
+		
+		$this->load->view('simple_view', $data);
+	}		
 	
 }

b8eb24ef27865a9a0c0f6b4bfcf3f8add4ce79e2
Doug Gleichman <douglas.gleichman@thinkom.com>
2018-01-09 09:29:14 -0800

    New base_url and index for FormGet

--- testci/application/config/config.php
+++ testci/application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/webdoug/testci/';
+//$config['base_url'] = 'http://localhost/webdoug/testci/';
+//$config['base_url']	= "http://{$_SERVER['HTTP_HOST']}/ThinKom/";
+$config['base_url']	= "http://{$_SERVER['HTTP_HOST']}/webdoug/testci/";
 
 /*
 |--------------------------------------------------------------------------

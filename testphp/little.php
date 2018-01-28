7c65c469d17e24734f59f40764be95310ce26287
Doug Gleichman <douglas.gleichman@thinkom.com>
2018-01-28 11:11:20 -0800

    Add php command line and sockets

---
+++ testphp/little.php
@@ -0,0 +1,9 @@
+<?php
+	echo "\nExample to be run from the command prompt\n";
+	echo "Add c:\\xampp\\php to path\n";		// that is c:\xampp\php
+	echo "Run program from command line: php little.php\n\n";
+	
+	$myStr = "Hello world!";
+	echo strlen($myStr) . "\n"; // outputs 12
+	echo "The length of " . $myStr . " is " . strlen($myStr) . "\n";
+?>
